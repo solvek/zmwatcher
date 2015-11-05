45eacf936fbfabd823a0aa17b54717fdc3f0f266
Solvek <sa@mobiwolf.com>
2015-11-05 09:01:37 +0200

    The first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+    /*
+    Watches for one or more cameras changes
+    parameters:
+    eventsPath - the path to events directory
+    cam1, cam2, cam3... - numbers of cameras to watch
+     */
+    watch: function(eventsPath){
+
+    }
+}
